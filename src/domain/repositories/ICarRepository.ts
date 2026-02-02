df1d0225148489b740b550f0f9b69604b965098b
Rafael <rafaelsouzaregis@gmail.com>
2026-02-01 23:02:38 -0400

    Interfaces

---
+++ src/domain/repositories/ICarRepository.ts
@@ -0,0 +1,8 @@
+import { car } from '../entities/car';
+
+export interface ICarRepository {
+    create(car: Car): Promise<void>;
+    findId(id: string): Promise<Car | null>;
+    findLicensePlate(licensePlate: string): Promise<Car | null>;
+    save(car: Car): Promise<void>;
+}
